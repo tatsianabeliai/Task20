fa61b488efc870a86e5f5db07803b20e6c3a157f
Tatiana Belyai <tatianabelyai@gmail.com>
2017-07-05 19:33:31 +0300

    corrected constant names

--- src/test/java/ClassOneTest.java
+++ src/test/java/ClassOneTest.java
@@ -5,12 +5,12 @@ import org.testng.annotations.*;
 
 public class ClassOneTest extends TxtDataProvider {
     private static final Logger LOG = Logger.getLogger(ClassOneTest.class);
-    private final static int numberOne = 50;
-    private final static int numberTwo = 5;
-    private final static int sumResult = 55;
-    private final static int deductResult = 45;
-    private final static int multiplyResult = 250;
-    private final static int divideResult = 10;
+    private final static int NUMBER_ONE = 50;
+    private final static int NUMBER_TWO = 5;
+    private final static int SUM_RESULT = 55;
+    private final static int DEDUCT_RESULT = 45;
+    private final static int MULTIPLY_RESULT = 250;
+    private final static int DIVIDE_RESULT = 10;
     private Mathematics mathematics;
 
     @BeforeClass(alwaysRun = true)
@@ -37,38 +37,38 @@ public class ClassOneTest extends TxtDataProvider {
 
     @Test(priority = 5, groups = {"smoke", "fast"})
     public void addTest() {
-        mathematics.add(numberOne, numberTwo);
+        mathematics.add(NUMBER_ONE, NUMBER_TWO);
         int result = mathematics.getResult();
-        Assert.assertEquals(sumResult, result, "Not expected result is displayed");
+        Assert.assertEquals(SUM_RESULT, result, "Not expected result is displayed");
     }
 
     @Test(priority = 3, timeOut = 500, groups = {"fast"}, dependsOnMethods = "addTest")
     public void deductTest() throws InterruptedException {
         Thread.sleep(1500);
-        mathematics.deduct(numberOne, numberTwo);
+        mathematics.deduct(NUMBER_ONE, NUMBER_TWO);
         int result = mathematics.getResult();
-        Assert.assertEquals(deductResult, result, "Not expected result is displayed");
+        Assert.assertEquals(DEDUCT_RESULT, result, "Not expected result is displayed");
     }
 
     @Test(priority = 4, groups = {"fast"})
     public void multiplyTest() {
-        mathematics.multiply(numberOne, numberTwo);
+        mathematics.multiply(NUMBER_ONE, NUMBER_TWO);
         int result = mathematics.getResult();
-        Assert.assertEquals(multiplyResult, result, "Not expected result is displayed");
+        Assert.assertEquals(MULTIPLY_RESULT, result, "Not expected result is displayed");
     }
 
     @Test(priority = 2, groups = {"fast"})
     public void divideTest() {
-        mathematics.divide(numberOne, numberTwo);
+        mathematics.divide(NUMBER_ONE, NUMBER_TWO);
         int result = mathematics.getResult();
-        Assert.assertEquals(divideResult, result, "Not expected result is displayed");
+        Assert.assertEquals(DIVIDE_RESULT, result, "Not expected result is displayed");
     }
 
     @Test(priority = 1, groups = {"fast"})
     public void divideByZeroTest() {
-        mathematics.divide(numberOne, 0);
+        mathematics.divide(NUMBER_ONE, 0);
         int result = mathematics.getResult();
-        Assert.assertEquals(divideResult, result);
+        Assert.assertEquals(DIVIDE_RESULT, result);
     }
 
     @Description("The test gets data from testngDDTTest.xml file. Run testngDDTTest.xml")
